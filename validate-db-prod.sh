fbb7286b2ca18d9cb8e989de36c7fa641b289b6f
raghubommi <86413956+raghubommi@users.noreply.github.com>
2022-01-03 00:33:49 +0530

    Update validate-db-prod.sh

--- validate-db-prod.sh
+++ validate-db-prod.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
  set -e
-
+ 
+ databaseName="pagila"
+ 
+ dbOutputJson=$(spawnctl get data-container $dbContainerName -o json)
+ pagilaHost=$(echo $dbOutputJson | jq -r '.host')
+ pagilaPort=$(echo $dbOutputJson | jq -r '.port')
+ pagilaUser=$(echo $dbOutputJson | jq -r '.user')
+ pagilaPassword=$(echo $dbOutputJson | jq -r '.password')
  docker run --net=host flyway/flyway -ignorePendingMigrations="true" validate -url="jdbc:postgresql://$pagilaHost:$pagilaPort/$databaseName" -user=$pagilaUser -password=$pagilaPassword
 
  echo "Successfully validated 'Pagila' database"
