39357a592d27d841370cf2a7db5e63202f342295
raghubommi <86413956+raghubommi@users.noreply.github.com>
2022-01-05 17:22:17 +0530

    Update migration-test-prod.sh

--- migration-test-prod.sh
+++ migration-test-prod.sh
@@ -4,4 +4,6 @@ docker-compose -f docker-compose-pg-flyway.yml down
 
 docker-compose -f docker-compose-pg-flyway.yml up 
 
+echo "completed running docker"
+
 docker run -it migrate /bin/sh -ignorePendingMigrations=true migrate 
