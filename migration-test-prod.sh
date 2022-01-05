a7f29ae9d589110935bbe2619e2db57e357dd5ef
raghubommi <86413956+raghubommi@users.noreply.github.com>
2022-01-05 17:30:41 +0530

    Update migration-test-prod.sh

--- migration-test-prod.sh
+++ migration-test-prod.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-pg-flyway.yml down
+#docker-compose -f docker-compose-pg-flyway.yml down
 
-docker-compose -f docker-compose-pg-flyway.yml up 
+#docker-compose -f docker-compose-pg-flyway.yml up 
+
+docker network create foo
+docker run -d --rm --name=postgres --network=foo postgres
+docker run -d --rm --name=flyway --network=foo flyway
 
 echo "completed running docker"
 
-docker run -it migrate /bin/sh -ignorePendingMigrations=true migrate 
+#docker run -it migrate /bin/sh -ignorePendingMigrations=true migrate 
