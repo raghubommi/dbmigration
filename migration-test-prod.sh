9ac0cec356ed67312b208f3c7246326150a4b1f9
raghubommi <86413956+raghubommi@users.noreply.github.com>
2022-01-05 17:50:45 +0530

    Update migration-test-prod.sh

--- migration-test-prod.sh
+++ migration-test-prod.sh
@@ -2,15 +2,17 @@
 
 #docker-compose -f docker-compose-pg-flyway.yml down
 
-#docker-compose -f docker-compose-pg-flyway.yml up 
+docker-compose -f docker-compose-pg-flyway.yml up 
 
-docker pull flyway/flyway > /dev/null 2>&1
+#docker pull flyway/flyway > /dev/null 2>&1
 
-docker pull postgres > /dev/null 2>&1
+#docker pull postgres > /dev/null 2>&1
 
-docker network create foo
-docker run -d --rm --name=postgres --network=foo postgres:latest
-docker run -d --rm --name=flyway --network=foo flyway/flyway
+#docker network create foo
+#docker run -d --rm --name=postgres --network=foo postgres:latest
+#docker run -d --rm --name=flyway --network=foo flyway/flyway
+
+#docker run --network=foo --rm -v $PWD/sql:/flyway/sql flyway/flyway -baselineOnMigrate="true" migrate -url="jdbc:postgresql://$pagilaHost:$pagilaPort/$databaseName" -user=$pagilaUser -password=$pagilaPassword
 
 echo "completed running docker"
 
